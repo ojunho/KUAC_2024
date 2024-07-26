e0993d66efd4d8e001b326fa4f92c9dfdc0307e5
ojunho <ojunho7535@gmail.com>
2024-07-26 13:01:24 +0900

    정적 장애물 launch 파일 생성

--- obstacle_detector/src/static_obstacle_avoidance.py
+++ obstacle_detector/src/static_obstacle_avoidance.py
@@ -1,46 +1,53 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
 import rospy
 from obstacle_detector.msg import Obstacles
 from std_msgs.msg import Int32
+from std_msgs.msg import Int32MultiArray
 import math
 
 class StaticAvoidance():
     def __init__(self):
         rospy.Subscriber("/raw_obstacles", Obstacles, self.obstacleCB)
+        rospy.Subscriber("/xycar_ultrasonic", Int32MultiArray, self.ultrasonicCB)
         self.obstacles = []
+        self.ultrasonic = [] # 왼쪽, 오른쪽, 오른쪽뒤, 뒤, 왼쪽뒤
         self.is_static = False
         self.steer = 0
+        self.is_left = False
 
         self.static_pub = rospy.Publisher("static_steer", Int32, queue_size=5)
 
         rate = rospy.Rate(10)
         while not rospy.is_shutdown():
-            if self.is_static:
-                if len(self.obstacles) == 0:
-                    # if 초음파 100 이상:
-                    #     self.is_static = False
-                    #     self.steer = 0
-                    pass
-            else:
-                if len(self.obstacles) < 3:
+            print("=======================")
+            print(f"{'static_obstacle' if self.is_static else 'lane_detection'}")
+            if self.is_static: # 장애물 주행 하다가
+                if len(self.obstacles) == 0: # 장애물이 안 잡히면
+                    print("초음파 활성화", end=" ")
+                    if self.is_left and self.ultrasonic[0] < 25:
+                        print("왼쪽에 아직 장애물 있음")
+                    elif not self.is_left and self.ultrasonic[1] < 20:
+                        print("오른쪽에 아직 장애물 있음")
+                        self.steer = 0
+                    else: # 장애물이 주변에 없음 - 정적 장애물 미션 끝
+                        self.is_static = False
+            else: # 일반 주행 하다가
+                if len(self.obstacles) > 0:
+                    self.is_static = True
                     tangent = self.obstacles[0][1] / self.obstacles[0][0]
                     degree = math.atan(tangent) * 180 / math.pi
-
-                    if degree > 0: # 왼쪽 장애물
-                        if degree < 30:
-                            self.steer = 30
-                        elif degree < 60:
-                            self.steer = -20
-                        else:
-                            self.steer = 0
+                    self.is_left = True if degree > 0 else False
+                    print(f"장애물 각도:{degree}")
+                    if self.is_left: # 왼쪽 장애물
+                        self.steer = 30
                     else:
-                        if degree > -30:
-                            self.steer = -30
-                        elif degree > -60:
-                            self.steer = 20
-                        else:
-                            self.steer = 0
+                        self.steer = -30
+            print(f"조향각:{self.steer}")
 
-            self.steer_pub.publish(self.steer)
+            self.static_pub.publish(self.steer)
+
             rate.sleep()
                         
 
@@ -55,6 +62,13 @@ class StaticAvoidance():
             self.obstacles = sorted(obstacles, key=lambda c: math.sqrt(c[0]**2 + c[1]**2))
         else:
             self.obstacles = []
+
+    def ultrasonicCB(self, msg):
+        ultrasonic = list(msg.data)
+        ultrasonic.pop(1)
+        ultrasonic.pop(1)
+        ultrasonic.pop(1)
+        self.ultrasonic = ultrasonic
 
 
 if __name__ == '__main__':
